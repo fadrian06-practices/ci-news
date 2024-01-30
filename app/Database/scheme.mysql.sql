ead64142b1b9a458984dbd7592faa8e375f030ac
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-30 15:21:12 -0400

    Connecting to database

---
+++ app/Database/scheme.mysql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE news (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title VARCHAR(128) NOT NULL,
+    slug VARCHAR(128) NOT NULL,
+    body TEXT NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE slug (slug)
+);
